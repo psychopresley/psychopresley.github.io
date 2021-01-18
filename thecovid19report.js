c22e9eee7ea6a000e80a21b98836f81e837ae9fa
psychopresley <psychopresley@gmail.com>
2021-01-18 16:00:46 -0300

    production commit

--- thecovid19report.js
+++ thecovid19report.js
@@ -1,6 +1,12 @@
-var confirmed_cases_menu = document.querySelector("#confirmed_cases_tab");
+var general_info_menu = document.querySelector("#world_data_tab");
 var active_cases_menu = document.querySelector("#active_cases_tab");
 var death_rate_menu = document.querySelector("#death_cases_tab");
+
+general_info_menu.addEventListener('click',function(){
+  var tab_content = document.querySelector("#tab_content");
+  tab_content.src = "tab_world_data.html";
+
+})
 
 active_cases_menu.addEventListener('click',function(){
   var tab_content = document.querySelector("#tab_content");
@@ -10,6 +16,6 @@ active_cases_menu.addEventListener('click',function(){
 
 death_rate_menu.addEventListener('click',function(){
   var tab_content = document.querySelector("#tab_content");
-  tab_content.src = "tab_death_rate.html";
+  tab_content.src = "death_cases_page.html";
 
 })
