2344dc11e55995165e0020d03722233d31a12f9d
psychopresley <psychopresley@gmail.com>
2021-01-21 14:31:15 -0300

    manual commit 21/01/2021

--- thecovid19report.js
+++ thecovid19report.js
@@ -1,35 +1,19 @@
-var world_data_menu = document.querySelector("#world_data_tab");
-var confirmed_cases_menu = document.querySelector("#confirmed_cases_tab");
-var active_cases_menu = document.querySelector("#active_cases_tab");
-var death_rate_menu = document.querySelector("#death_cases_tab");
-var read_me_page_menu = document.querySelector("#read_me_page_tab");
-
-world_data_menu.addEventListener('click',function(){
-  var tab_content = document.querySelector("#tab_content");
-  tab_content.src = "world_data_page.html";
-
+$('#world_data_tab').click(function(){
+  $('#tab_content').attr('src','world_data_page.html')
 })
 
-confirmed_cases_menu.addEventListener('click',function(){
-  var tab_content = document.querySelector("#tab_content");
-  tab_content.src = "confirmed_cases_page.html";
-
+$('#confirmed_cases_tab').click(function(){
+  $('#tab_content').attr('src','confirmed_cases_page.html')
 })
 
-active_cases_menu.addEventListener('click',function(){
-  var tab_content = document.querySelector("#tab_content");
-  tab_content.src = "active_cases_page.html";
-
+$('#active_cases_tab').click(function(){
+  $('#tab_content').attr('src','active_cases_page.html')
 })
 
-death_rate_menu.addEventListener('click',function(){
-  var tab_content = document.querySelector("#tab_content");
-  tab_content.src = "death_cases_page.html";
-
+$('#death_cases_tab').click(function(){
+  $('[id=tab_content]').attr('src','death_cases_page.html')
 })
 
-read_me_page_menu.addEventListener('click',function(){
-  var tab_content = document.querySelector("#tab_content");
-  tab_content.src = "read_me_page.html";
-
+$('#read_me_page_tab').click(function(){
+  $('[id=tab_content]').attr('src','read_me_page.html')
 })
